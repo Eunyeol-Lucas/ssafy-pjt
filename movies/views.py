e32fb87b024e2617e9c6d02d172a5f07b7c91606
Eunyeol <ney9083@gmail.com>
2022-03-04 21:43:01 +0900

    pjt04 제출

--- movies/views.py
+++ movies/views.py
@@ -17,11 +17,11 @@ def index(request):
     response  = requests.get(BASE_URL, params=params )
     data = response.json()
     if data.get("results"):
-        movie_data = data.get("results")[:9]
+        movie_data = data.get("results")[:8]
         
         for i in movie_data:
             tmp ={
-                "id": i.get("poster_path")[1:],
+                "id": f'movie{i.get("id")}',
                 "title": i.get("title"),
                 "overview": i.get("overview"),
                 "release_date": i.get("release_date"),
@@ -69,6 +69,7 @@ def recommendations(request):
                         "site": f'https://www.themoviedb.org/movie/{i.get("id")}',
                         "release_date": i.get("release_date"),
                         "overview": i.get("overview"),
+                        "vote": round(i.get("vote_average"), 1)
                     }
                     recommendation_movie_list.append(tmp)
 
@@ -80,8 +81,41 @@ def recommendations(request):
 
 
 def test():
-    num = "/wPU78OPN4BYEgWYdXyg0phMee64.jpg"
-    print(num[1:])
-    
+    recommendation_movie_list = []
+    BASE_URL = 'https://api.themoviedb.org/3'
+    path = '/search/movie'
+    params1 = {
+        'api_key': 'eba1de962633cb7eb4cb4be4665bf5ba',
+        'region': 'KR',
+        'language': "ko",
+        'query': "쇼생크 탈출"
+    }
+    params2 = {
+        'api_key': 'eba1de962633cb7eb4cb4be4665bf5ba',
+        'region': 'KR',
+        'language': "ko"
+    }
+    response = requests.get(BASE_URL+path, params=params1)
+    data = response.json()
+    results = data.get('results')
+    pprint(data)
+    if results:
+        movie_id = results[0].get('id')
+        if movie_id:
+            recommendation_path = f'/movie/{movie_id}/recommendations'
+            recommendation_response = requests.get(
+                BASE_URL+recommendation_path, params=params2).json()
+            recommendation_results = recommendation_response.get('results')
+            if recommendation_results:
+                for i in recommendation_results:
+                    tmp = {
+                        "title": i.get("title"),
+                        "poster": f'https://image.tmdb.org/t/p/original{i.get("poster_path")}',
+                        "site": f'https://www.themoviedb.org/movie/{i.get("id")}',
+                        "release_date": i.get("release_date"),
+                        "overview": i.get("overview"),
+                        "vote": round(i.get("vote_average"), 1)
+                    }
+                    recommendation_movie_list.append(tmp)
 
 test()
