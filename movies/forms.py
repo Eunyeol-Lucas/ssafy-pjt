edb3ba68f1828ca9cd2ff59b038a1fc286383706
Eunyeol <ney9083@gmail.com>
2022-04-08 14:31:02 +0900

    Feat: ModelForm 기능 및 디자인 구현

--- movies/forms.py
+++ movies/forms.py
@@ -1,8 +1,89 @@
+from platform import release
+from tkinter import Widget
+from turtle import width
 from django import forms
 from .models import Movie
 
 class MovieForm(forms.ModelForm):
+    GENRE_A = '코미디'
+    GENRE_B = '공포'
+    GENRE_C = '로맨스'
+    GENRE_CHOICES = [
+        (GENRE_A, '코미디'),
+        (GENRE_B, '공포'),
+        (GENRE_C, '로맨스'),
+    ]
+    
+    title = forms.CharField(
+        label="Title",
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': "Title",
+                "class":"form-control" 
+            }
+        ),
+    )
+    audience = forms.IntegerField(
+        label="Audience",
+        widget=forms.NumberInput(
+            attrs={
+                'placeholder': 'audience',
+                "class":"form-control" 
+            }
+        )
+    )
+    release_date = forms.DateTimeField(
+        label="Release_date",
+        widget=forms.DateInput(
+            attrs={
+                'type':'date',
+                "class":"form-control" 
+            }
+        )
+    )
+    genre = forms.ChoiceField(
+        label="Genre",
+        choices=GENRE_CHOICES,
+        widget=forms.Select(
+            attrs={
+                "class":"form-control" 
+            }
+        )
+    )
+    score = forms.IntegerField(
+        label="Score",
+        widget=forms.NumberInput(
+            attrs={
+                'type': 'number',
+                'placeholder': 'Score',
+                "class":"form-control" ,
+                'max': 5,
+                'min': 0.5,
+                'step':0.5,
+            }
+        )
+    )
+    poster_url = forms.CharField(
+        label="Poster url",
+        widget = forms.TextInput(
+            attrs={
+                'placeholder':'Poster url',
+                "class":"form-control" ,
+            }
+        )
+    )
+    description = forms.CharField(
+        label="Description",
+        widget=forms.Textarea(
+            attrs={
+                'placeholder':'Description',
+                "class":"form-control" 
+            }
+        )
+    )
+    
     class Meta:
         model = Movie
-        fields = ('title', 'audience', 'release_date', 'genre', 'score', 'post_url', 'description')
+        fields = ('title', 'audience', 'release_date', 'genre', 'score', 'poster_url', 'description')
+
     
--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import get_object_or_404, render
+from django.views.decorators.http import require_http_methods
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Movie
+from .forms import MovieForm
 
 # Create your views here.
 def index(request):
@@ -8,3 +10,17 @@ def index(request):
         'movie_list': movie_list,
     }
     return render(request, 'movies/index.html', context)
+
+@require_http_methods(['GET', 'POST'])
+def create(request):
+    if request.method == "POST":
+        form = MovieForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('movies:index')
+    else:
+        form = MovieForm()
+    context = {
+        'form' : form
+    }
+    return render(request, 'movies/create.html', context)
