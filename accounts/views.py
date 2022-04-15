b8942f35674a9d8bb11722806dad239211dd55c0
SilverLight96 <tim960321@naver.com>
2022-04-15 14:18:30 +0900

    Commit signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .forms import CustomUserCreationForm, UserCreationForm
+from django.contrib.auth import login
 # Create your views here.
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('movies:index')
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request,user)
+            return redirect('movies:index')
+    else:
+        form = CustomUserCreationForm()
+    
+    context = {
+        'form':form,
+    }
+    return render(request,'accounts/signup.html',context)
