2d047b4ad1f01d5e2668a88f2e601b3c89dfe442
Eunyeol <ney9083@gmail.com>
2022-04-15 17:50:40 +0900

    Feat: 영화 등록, 업데이트 기능 구현

---
+++ movies/forms.py
@@ -0,0 +1,12 @@
+from .models import Movie, Comment
+from django import forms
+
+class MovieForm(forms.ModelForm):
+    class Meta:
+        model = Movie
+        fields = ('title', 'description', )
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('content',)
--- movies/urls.py
+++ movies/urls.py
@@ -3,4 +3,8 @@ from . import views
 app_name = 'movies'
 urlpatterns = [
     path('/', views.index,name='index'),
+    path('create/', views.create, name="create"),
+    path('<int:pk>/', views.detail, name="detail"),
+    path('<int:pk>/update', views.update, name="update"),
+
 ]
