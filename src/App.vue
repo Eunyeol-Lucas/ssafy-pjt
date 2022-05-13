730b8971125571103cc27321699bb7bb526a7cfa
Eunyeol <ney9083@gmail.com>
2022-05-13 13:29:14 +0900

    api key 추가

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <div id="app">
     <nav>
-      <router-link :to="{name: 'home'}">Home</router-link> |
-      <router-link :to="{name: 'random'}">Random</router-link>
-      <router-link :to="{name: 'watchList'}"></router-link>
+      <router-link :to="{ name: 'home' }">Home</router-link> |
+      <router-link :to="{ name: 'random' }">Random</router-link>
+      <router-link :to="{ name: 'watchList' }"></router-link>
     </nav>
-    <router-view/>
+    <router-view />
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      API_KEY: "eba1de962633cb7eb4cb4be4665bf5ba",
+    };
+  },
+};
+</script>
 
-<style>
-
-</style>
+<style></style>
