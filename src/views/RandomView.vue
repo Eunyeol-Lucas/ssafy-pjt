f306e8e7a96e4eeebf6b81c4a04b50dce3fa12c2
Eunyeol <ney9083@gmail.com>
2022-05-13 13:08:09 +0900

    Initialization

---
+++ src/views/RandomView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>RandomView</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/WatchListView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>WatchListView</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
